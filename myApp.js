9648969ed004a4b5855fbe4b5ae6812c892d6e12
Victor <vlougongs@gmail.com>
2024-01-22 19:51:30 +0000

    Exercise 2: Fix!

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const { Schema } = mongoose;
