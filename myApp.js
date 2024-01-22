b30db502b0f8e82e2a22f983a6a4ed9d7436d362
Victor <vlougongs@gmail.com>
2024-01-22 20:46:44 +0000

    Exercise 4: Fix 5!

--- myApp.js
+++ myApp.js
@@ -21,10 +21,13 @@ const createAndSavePerson = (done) => {
   });
 };
 
+arrayOfPeople = [{ name: 'Juan', age: 26, favoriteFoods: ['Tacos', 'Quesadilla'] }, { name: 'Juan', age: 26, favoriteFoods: ['Tacos', 'Quesadilla'] }, { name: 'Juan', age: 26, favoriteFoods: ['Tacos', 'Quesadilla'] }, { name: 'Juan', age: 26, favoriteFoods: ['Tacos', 'Quesadilla'] }];
+
 const createManyPeople = (arrayOfPeople, done) => {
-  arrayOfPeople = [{ name: 'Juan', age: 26, favoriteFoods: ['Tacos', 'Quesadilla'] }, { name: 'Juan', age: 26, favoriteFoods: ['Tacos', 'Quesadilla'] }, { name: 'Juan', age: 26, favoriteFoods: ['Tacos', 'Quesadilla'] }, { name: 'Juan', age: 26, favoriteFoods: ['Tacos', 'Quesadilla'] }];
-  Person.create(arrayOfPeople);
-  done(null, data);
+  Person.create(arrayOfPeople, function (err, data) {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
