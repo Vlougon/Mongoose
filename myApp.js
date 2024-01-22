c668467da38396a2b664c18f3984d5400bcc6185
Victor <vlougongs@gmail.com>
2024-01-22 21:10:08 +0000

    Exercise 5: Fix 6!

--- myApp.js
+++ myApp.js
@@ -30,11 +30,11 @@ const createManyPeople = (arrayOfPeople, done) => {
   });
 };
 
-personName = { name: 'Juan' };
-
 const findPeopleByName = (personName, done) => {
-  Person.find(personName);
-  done(null, data);
+  Person.find({ name: personName }, function (err, data) {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
