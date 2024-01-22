b7ef1678c320ed058baf65f3379434abe49e1912
Victor <vlougongs@gmail.com>
2024-01-22 19:47:55 +0000

    Exercise 2: Done!

--- myApp.js
+++ myApp.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
-let mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const { Schema } = mongoose;
 
-let Person;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+let Person = mongoose.Model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
