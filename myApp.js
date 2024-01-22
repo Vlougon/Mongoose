9f9b4e6248349d6f72f0131d70747439bc140ffc
Victor <vlougongs@gmail.com>
2024-01-22 22:10:08 +0000

    Exercise 12: Fix 3!

--- myApp.js
+++ myApp.js
@@ -92,7 +92,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({ favoriteFoods: [foodToSearch] }).sort({ name: 1 }).limit(2).select({ age: 0 }).exec(function (err, data) {
+  Person.find({ favoriteFoods: foodToSearch }).sort({ name: 1 }).limit(2).select({ age: 0 }).exec(function (err, data) {
     if (err) return done(err);
     done(null, data);
   });
