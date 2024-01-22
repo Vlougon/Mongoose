9d8297a353a1795d8f9a85ea10e3470acaa273d5
Victor <vlougongs@gmail.com>
2024-01-22 21:18:56 +0000

    Exercise 7: Alternative!

--- myApp.js
+++ myApp.js
@@ -45,7 +45,7 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  Person.findById({ _id: personId }, function (err, data) {
+  Person.findById(personId, function (err, data) {
     if (err) return done(err);
     done(null, data);
   });
